3e9add0b41303d10ac41d7cb3c3daa8262e79181
mgatzke11 <mgatzke@compun.com>
2025-01-29 11:12:01 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Timeclock App</h1>
+      <p>Welcome to the timeclock system.</p>
+    </div>
+  );
+}
+
+export default App;
